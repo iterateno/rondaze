54283f1a959620f5d7f8add3ccb73279aff9ae17
Thomas Rye <thomasjrye@gmail.com>
2025-07-17 15:20:21 +0200

    api_url from .env

--- src/SaveHighScoreModal.jsx
+++ src/SaveHighScoreModal.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './SaveHighScoreModal.css';
 
-const API_URL = 'http://localhost:3002/api/highscores';
+const API_URL = process.env.REACT_APP_API_URL + '/api/highscores';
 
 const SaveHighScoreModal = ({ score, refetch }) => {
     const [name, setName] = useState('');
